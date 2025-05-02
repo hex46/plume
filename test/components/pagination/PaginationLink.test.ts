e02b12fcad3c8bee84cdb66d7b98a3cb719be423
Florent A. <fa+git@hex46.fr>
2025-05-02 22:41:30 +0200

    :white_check_mark: Add PaginationLink.test.ts

---
+++ test/components/pagination/PaginationLink.test.ts
@@ -0,0 +1,48 @@
+import { experimental_AstroContainer as AstroContainer } from "astro/container";
+import { describe, expect, test } from "vitest";
+// @ts-ignore
+import PaginationLink from "@/components/pagination/PaginationLink.astro";
+
+describe("PaginationLink", () => {
+  test('should not show link when "link" prop is undefined', async () => {
+    const container = await AstroContainer.create();
+    const result = await container.renderToString(PaginationLink);
+
+    expect(result).not.toContain("</a>");
+  });
+
+  test('should show label when "link" prop is undefined', async () => {
+    const container = await AstroContainer.create();
+    const result = await container.renderToString(PaginationLink, {
+      props: {
+        label: "Hello there!",
+      },
+    });
+
+    expect(result).toContain("Hello there!");
+  });
+
+  test('should show link when "link" prop is defined', async () => {
+    const container = await AstroContainer.create();
+    const result = await container.renderToString(PaginationLink, {
+      props: {
+        link: "http://example.com",
+        label: "Hello there!",
+      },
+    });
+
+    expect(result).toContain('href="http://example.com"');
+  });
+
+  test('should show label when "link" prop is defined', async () => {
+    const container = await AstroContainer.create();
+    const result = await container.renderToString(PaginationLink, {
+      props: {
+        link: "http://example.com",
+        label: "Hello there!",
+      },
+    });
+
+    expect(result).toContain("Hello there!");
+  });
+});
