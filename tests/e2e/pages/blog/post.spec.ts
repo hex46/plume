5b178ce130ff58cfbc15e08c4909b6ff56d620c4
Florent A. <fa+git@hex46.fr>
2025-05-03 21:56:33 +0200

    :white_check_mark: Add blog tests (pagination & posts)

---
+++ tests/e2e/pages/blog/post.spec.ts
@@ -0,0 +1,19 @@
+import { test, expect } from "@playwright/test";
+
+test.describe("blog posts", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("/blog");
+  });
+
+  test("has 2 blog posts", async ({ page }) => {
+    expect(await page.getByRole("article").count()).toBe(2);
+  });
+
+  test("navigate to first blog post", async ({ page }) => {
+    const firstBlogPostLink = await page.getByRole("link", {
+      name: "Lorem Ipsum - 15",
+    });
+    await firstBlogPostLink.click();
+    await page.waitForURL("blog/lorem_ipsum_15");
+  });
+});
