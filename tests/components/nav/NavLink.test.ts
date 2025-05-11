01ef26b43317ac0c91d7c7d73965d96a51846111
Florent A. <fa+git@hex46.fr>
2025-05-11 18:15:40 +0200

    :bug: Fix active class for nav

--- tests/components/nav/NavLink.test.ts
+++ tests/components/nav/NavLink.test.ts
@@ -2,7 +2,7 @@ import { experimental_AstroContainer as AstroContainer } from "astro/container";
 import { describe, expect, test } from "vitest";
 // @ts-ignore
 import NavLink from "@/components/nav/NavLink.astro";
-import type { NavElement } from "@/types/Card.ts";
+import type { NavElement } from "@/types/Navigation.ts";
 
 describe("NavLink", () => {
   test("should display link", async () => {
@@ -22,22 +22,25 @@ describe("NavLink", () => {
     expect(result).toContain(`href="${item.url}"`);
   });
 
-  test("should display an active link if the current page URL matches", async () => {
-    const item = {
-      title: "Link",
-      url: "/blog",
-    } as NavElement;
+  test.each(["/blog/", "/blog/1", "/blog/lorem_ipsum_14"])(
+    "should display an active link if the current page URL matches",
+    async (path) => {
+      const item = {
+        title: "Link",
+        url: "/blog",
+      } as NavElement;
 
-    const container = await AstroContainer.create();
-    const result = await container.renderToString(NavLink, {
-      props: {
-        item,
-      },
-      request: new Request("https://example.com/blog"),
-    });
+      const container = await AstroContainer.create();
+      const result = await container.renderToString(NavLink, {
+        props: {
+          item,
+        },
+        request: new Request("https://example.com" + path),
+      });
 
-    expect(result).toContain('class="active"');
-  });
+      expect(result).toContain('class="active"');
+    },
+  );
 
   test("should NOT display an active link if the current page URL doesn't matches", async () => {
     const item = {
