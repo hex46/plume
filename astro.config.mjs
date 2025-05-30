afeae94b02eef14eeb8ae7d8816b18d77f86e6b9
Florent A. <fa+git@hex46.fr>
2025-05-30 15:09:01 +0200

    :sparkles: Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [],
   },
+  site: 'http://localhost:4321',
+  integrations: [sitemap()],
 });
